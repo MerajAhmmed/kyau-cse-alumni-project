f3e8ecb1c38fb61beb785e25e4a7a69812091c0c
MerajAhmmed <merajahmmed6@gmail.com>
2022-08-12 22:59:21 +0600

    regitratiion file on going

---
+++ Register.php
@@ -0,0 +1,75 @@
+<?php
+ echo "<link rel='stylesheet' type='text/css' href='style.css'>";
+?>
+
+
+
+
+<section id="Register">
+    <div class="main-login-div">
+        <div class="allinput">
+            <div class="headerpart">
+                <h1>Register</h1>
+                <p>Register and enjoy our alumni association website</p>
+                <p>Already have an <a href="./login.php">account</a>
+                </p>
+            </div>
+            <div>
+                <div>
+                    <div class="logo"> 
+                        <img src="./image/kyau.jpg" alt="camera">
+                    </div>
+                    <img src="" alt="camera" id="logo1">
+                    <small>Choose image</small>
+                    <input type="file" form="" class="" id="" name="">
+                </div>
+                <div class="registration-input">
+                    <form action="./Register.php">
+                        <div class="First-name">
+                            <input type="text" placeholder="First name">
+                        </div>
+                        <div>
+                            <input type="text" placeholder="last name">
+                        </div>
+
+                        <div>
+                            <input type="text" placeholder="username">
+                        </div>
+                        <div>
+                            <input type="password" placeholder="password">
+                        </div>
+                        <div>
+                            <input type="password" placeholder="Confirm password">
+                        </div>
+                        <div>
+                            <input type="email" placeholder="Email">
+                        </div>
+                        <div>
+                            <input type="text" placeholder="date of Birth">
+                        </div>
+                        <div>
+                            <input type="text" placeholder="Select Blood Group">
+                        </div>
+                        <div>
+                            <input type="text" placeholder="Department">
+                        </div>
+                        <div>
+                            <input type="text" placeholder="Select degree">
+                        </div>
+                        <div>
+                            <input type="text" placeholder="Year of Graduation ">
+                        </div>
+                        <div>
+                            <input type="text" placeholder="Phone number">
+                        </div>
+                        <div>
+                            <input type="Checkbox" placeholder="Department">
+                        </div class="registerbtn">
+                        <button type="submit" class="registertbtn" >Register</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
